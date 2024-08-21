6f287c27b046733fc0217a50eefc832ea210681d
Thibaut EhlingerPro <thibautehlingerpro@Thibauts-MacBook-Pro.local>
2024-08-21 16:33:12 +0200

    Objets cube et face tout basiques avant les rotation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # the-cube
 Simulateur de rubik's
+TODO :
+le plus dur va venir là. Il va falloir coder les fonctions de rotation : 
+	1. "R", "R'", "U", "U'", "F", "F'"
+	2. "x", "y", "z"
+	3. Toutes les manquantes en 1 à partir de combinaisons de 1 et de 2
--- script.py
+++ script.py
@@ -1,1 +1,57 @@
-print("Hello world")
+class Face:
+    def __init__(self):
+        self.cells = [None for i in range(0,9)]
+
+    def __init__(self,color):
+        if color not in "WOGRBY":
+            raise ValueError("Argument is not a color : " + str(color))
+        self.cells = [color for i in range(0,9)]
+
+    def __str__(self):
+        formatted_str = ""
+        for i in range(0,3):
+            for j in range(0,3):
+                formatted_str = formatted_str + self.cells[i*3+j]
+            formatted_str = formatted_str + "\n" 
+        return formatted_str
+
+        return ''.join(self.cells)
+
+class Cube:
+    def __init__(self):
+        self.down = Face("W")
+        self.front = Face("O")
+        self.left = Face("G")
+        self.back = Face("R")
+        self.right = Face("B")
+        self.up = Face("Y") 
+
+    def __str__(self):
+        #PRINT UP
+        res = ""
+        for i in range(0,3):
+            res = res + "    "
+            for j in range(0,3):
+                res = res + str(self.up.cells[3*i+j])
+            res += "\n"
+        #PRINT MID
+        for i in range(0,3):
+            for j in range(0,3):
+                res = res + str(self.left.cells[3*i+j])
+            res += " " 
+            for j in range(0,3):
+                res = res + str(self.front.cells[3*i+j])
+            res += " " 
+            for j in range(0,3):
+                res = res + str(self.right.cells[3*i+j])
+            res += " " 
+            for j in range(0,3):
+                res = res + str(self.back.cells[3*i+j])
+            res += "\n"
+        #PRINT DOWN
+        for i in range(0,3):
+            res = res + "    "
+            for j in range(0,3):
+                res = res + str(self.down.cells[3*i+j])
+            res += "\n"
+        return res
